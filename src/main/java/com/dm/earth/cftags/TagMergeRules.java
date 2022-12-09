3fe36cbd1044ecb5ce436cb9d7e17acedda2688f
Yjn024 <jiening.yu@outlook.com>
2022-12-09 18:27:37 +0800

    ores

--- src/main/java/com/dm/earth/cftags/TagMergeRules.java
+++ src/main/java/com/dm/earth/cftags/TagMergeRules.java
@@ -5,10 +5,13 @@ import static com.dm.earth.cftags.TagMergeRule.register;
 public class TagMergeRules {
     public static final TagMergeRule INGOTS = register(new TagMergeRule.Simple("ingots"));
     public static final TagMergeRule PLATES = register(new TagMergeRule.Simple("plates"));
+    public static final TagMergeRule TINY_DUSTS = register(new TagMergeRule.Simple("tiny_dusts"));
     public static final TagMergeRule SMALL_DUSTS = register(new TagMergeRule.Simple("small_dusts"));
     public static final TagMergeRule DUSTS = register(new TagMergeRule.Simple("dusts"));
     public static final TagMergeRule NUGGETS = register(new TagMergeRule.Simple("nuggets"));
     public static final TagMergeRule RODS = register(new TagMergeRule.Simple("rods"));
+
+    public static final TagMergeRule ORES = register(new TagMergeRule.Simple("ores"));
 
     public static void load() {
     }
