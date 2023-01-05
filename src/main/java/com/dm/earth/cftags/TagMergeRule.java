490a0f4f8808d2954fe7d4ae775af62469e3bacc
JieningYu <61787533+JieningYu@users.noreply.github.com>
2023-01-05 13:11:55 +0800

    bump to 0.1.2

--- src/main/java/com/dm/earth/cftags/TagMergeRule.java
+++ src/main/java/com/dm/earth/cftags/TagMergeRule.java
@@ -11,134 +11,139 @@ import org.jetbrains.annotations.Nullable;
 import net.minecraft.util.Identifier;
 
 public interface TagMergeRule {
-    static final ArrayList<TagMergeRule> RULES = new ArrayList<>();
+	static final ArrayList<TagMergeRule> RULES = new ArrayList<>();
 
-    static <T extends TagMergeRule> T register(T rule) {
-        RULES.add(rule);
-        return rule;
-    }
+	static <T extends TagMergeRule> T register(T rule) {
+		RULES.add(rule);
+		return rule;
+	}
 
-    @Nullable
-    String ftc(String forgeTag);
+	@Nullable
+	String ftc(String forgeTag);
 
-    @Nullable
-    String ctf(String commonTag);
+	@Nullable
+	String ctf(String commonTag);
 
-    @NotNull
-    default Optional<Identifier> ctf(Identifier commonTag) {
-        String f = this.ctf(commonTag.getPath());
-        return f == null || f.equals(commonTag.getPath()) ? Optional.empty() : Optional.of(new Identifier("c", f));
-    }
+	@NotNull
+	default Optional<Identifier> ctf(Identifier commonTag) {
+		String f = this.ctf(commonTag.getPath());
+		return (!commonTag.getNamespace().equals("c")) || f == null || f.equals(commonTag.getPath())
+				? Optional.empty()
+				: Optional.of(new Identifier("c", f));
+	}
 
-    @NotNull
-    default Optional<Identifier> ftc(Identifier forgeTag) {
-        String c = this.ftc(forgeTag.getPath());
-        return c == null || c.equals(forgeTag.getPath()) ? Optional.empty() : Optional.of(new Identifier("c", c));
-    }
+	@NotNull
+	default Optional<Identifier> ftc(Identifier forgeTag) {
+		String c = this.ftc(forgeTag.getPath());
+		return (!forgeTag.getNamespace().equals("c")) || c == null || c.equals(forgeTag.getPath())
+				? Optional.empty()
+				: Optional.of(new Identifier("c", c));
+	}
 
-    boolean isHighPriority();
+	boolean isHighPriority();
 
-    static class Common implements TagMergeRule {
-        protected final String matchingPartForge;
-        protected final String matchingPartCommon;
+	static class Common implements TagMergeRule {
+		protected final String matchingPartForge;
+		protected final String matchingPartCommon;
 
-        public Common(String cPart, String fPart) {
-            this.matchingPartForge = fPart;
-            this.matchingPartCommon = cPart;
-        }
+		public Common(String cPart, String fPart) {
+			this.matchingPartForge = fPart;
+			this.matchingPartCommon = cPart;
+		}
 
-        @Override
-        public @Nullable String ctf(String commonTag) {
-            ArrayList<String> parts = new ArrayList<>(List.of(commonTag.split("_")));
-            if (commonTag.contains(matchingPartCommon) && parts.size() >= 2 + getSubCount(matchingPartCommon, "_")) {
-                Collections.reverse(parts);
-                return matchingPartForge + "/" + commonTag.replaceAll("_" + matchingPartCommon, "");
-            }
-            return null;
-        }
+		@Override
+		public @Nullable String ctf(String commonTag) {
+			ArrayList<String> parts = new ArrayList<>(List.of(commonTag.split("_")));
+			if (commonTag.contains(matchingPartCommon)
+					&& parts.size() >= 2 + getSubCount(matchingPartCommon, "_")) {
+				Collections.reverse(parts);
+				return matchingPartForge + "/" + commonTag.replaceAll("_" + matchingPartCommon, "");
+			}
+			return null;
+		}
 
-        @Override
-        public boolean isHighPriority() {
-            return this.matchingPartCommon.contains("_");
-        }
+		@Override
+		public boolean isHighPriority() {
+			return this.matchingPartCommon.contains("_");
+		}
 
-        @Override
-        public @Nullable String ftc(String forgeTag) {
-            ArrayList<String> parts = new ArrayList<>();
-            parts.addAll(List.of(forgeTag.split("/")));
-            if (forgeTag.contains(matchingPartForge) && parts.size() >= 2) {
-                Collections.reverse(parts);
-                parts.replaceAll(in -> in.equals(matchingPartForge) ? matchingPartCommon : in);
-                return String.join("_", parts.toArray(new String[0]));
-            }
-            return null;
-        }
+		@Override
+		public @Nullable String ftc(String forgeTag) {
+			ArrayList<String> parts = new ArrayList<>();
+			parts.addAll(List.of(forgeTag.split("/")));
+			if (forgeTag.contains(matchingPartForge) && parts.size() >= 2) {
+				Collections.reverse(parts);
+				parts.replaceAll(in -> in.equals(matchingPartForge) ? matchingPartCommon : in);
+				return String.join("_", parts.toArray(new String[0]));
+			}
+			return null;
+		}
 
-        private static int getSubCount(String main, String sub) {
-            return (main.length() - main.replaceAll(sub, "").length()) / sub.length();
-        }
-    }
+		private static int getSubCount(String main, String sub) {
+			return (main.length() - main.replaceAll(sub, "").length()) / sub.length();
+		}
+	}
 
-    static class Simple extends Common {
-        public Simple(String part) {
-            super(part, part);
-        }
-    }
+	static class Simple extends Common {
+		public Simple(String part) {
+			super(part, part);
+		}
+	}
 
-    static class Custom implements TagMergeRule {
-        protected final SingleRule ftcRule;
-        protected final SingleRule ctfRule;
+	static class Custom implements TagMergeRule {
+		protected final SingleRule ftcRule;
+		protected final SingleRule ctfRule;
 
-        public Custom(SingleRule ftc, SingleRule ctf) {
-            this.ftcRule = ftc;
-            this.ctfRule = ctf;
-        }
+		public Custom(SingleRule ftc, SingleRule ctf) {
+			this.ftcRule = ftc;
+			this.ctfRule = ctf;
+		}
 
-        @Override
-        public @Nullable String ftc(String forgeTag) {
-            return ftcRule.convert(forgeTag);
-        }
+		@Override
+		public @Nullable String ftc(String forgeTag) {
+			return ftcRule.convert(forgeTag);
+		}
 
-        @Override
-        public @Nullable String ctf(String commonTag) {
-            return ctfRule.convert(commonTag);
-        }
+		@Override
+		public @Nullable String ctf(String commonTag) {
+			return ctfRule.convert(commonTag);
+		}
 
-        @Override
-        public boolean isHighPriority() {
-            return false;
-        }
+		@Override
+		public boolean isHighPriority() {
+			return false;
+		}
 
-        @FunctionalInterface
-        public static interface SingleRule {
-            @Nullable
-            String convert(String tag);
-        }
-    }
+		@FunctionalInterface
+		public static interface SingleRule {
+			@Nullable
+			String convert(String tag);
+		}
+	}
 
-    static class Direct implements TagMergeRule {
-        protected final String forge;
-        protected final String common;
+	static class Direct implements TagMergeRule {
+		protected final String forge;
+		protected final String common;
 
-        public Direct(String common, String forge) {
-            this.common = common;
-            this.forge = forge;
-        }
+		public Direct(String common, String forge) {
+			this.common = common;
+			this.forge = forge;
+		}
 
-        @Override
-        public @Nullable String ftc(String forgeTag) {
-            return forgeTag.equals(forge) ? common : null;
-        }
+		@Override
+		public @Nullable String ftc(String forgeTag) {
+			return forgeTag.equals(forge) ? common : null;
+		}
 
-        @Override
-        public @Nullable String ctf(String commonTag) {
-            return commonTag.equals(common) ? forge : common;
-        }
+		@Override
+		public @Nullable String ctf(String commonTag) {
+			return commonTag.equals(common) ? forge : common;
+		}
 
-        @Override
-        public boolean isHighPriority() {
-            return true;
-        }
+		@Override
+		public boolean isHighPriority() {
+			return true;
+		}
 
-    }
+	}
 }
